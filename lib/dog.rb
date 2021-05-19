7930343b6a1b2c9b0d07b1601aa6e9b1f1ad5b7e
Lena Hou <lenahou2013@gmail.com>
2021-05-19 21:31:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -51,10 +51,13 @@ def self.new_from_db(row)
 end
 
 def self.find_by_id(id)
-sql = "SELECT * FROM dogs WHERE id = ?"
-result = DB[:conn].execute(sql, id)[0]
-Dog.new(result[0], result[1], result[2])
-end
+    sql = <<-SQL
+      SELECT * FROM dogs WHERE id = ?
+    SQL
+    result = DB[:conn].execute(sql, id).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
 
 def self.find_or_create_by(name:, breed:)
   dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = '#{name}'AND breed = '{breed}'")
