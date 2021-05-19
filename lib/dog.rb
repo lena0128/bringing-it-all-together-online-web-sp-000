5b4ffb5a0a11be53de0b512181032d2add1750d8
Lena Hou <lenahou2013@gmail.com>
2021-05-19 21:10:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -52,6 +52,12 @@ def self.new_from_db(row)
   new_dog
 end
 
+def self.find_by_id(id)
+sql = "SELECT * FROM dogs WHERE id = ?"
+result = DB[:conn].execute(sql, id)[0]
+Dog.new(result[0], result[1], result[2])
+end
+
 def update
 sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ? "
 DB[:conn].execute(sql, self.name, self.breed, self.id)
