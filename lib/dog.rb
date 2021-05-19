371b6d61d861ad90c6dc74174b6244da88105589
Lena Hou <lenahou2013@gmail.com>
2021-05-19 21:31:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -46,6 +46,10 @@ def self.create(dog_attributes)
   dog
 end
 
+def self.new_from_db(row)
+  self.new(id:row[0], name:row[1], breed:row[2])
+end
+
 def self.find_by_id(id)
 sql = "SELECT * FROM dogs WHERE id = ?"
 result = DB[:conn].execute(sql, id)[0]
@@ -60,14 +64,6 @@ def self.find_or_create_by(name:, breed:)
   else
     dog = self.create(name:name, breed:breed)
   end
-  dog
-end
-
-def self.new_from_db(row)
-  id = row[0]
-  name = row[1]
-  breed = row[2]
-  dog = self.new(id, name, breed)
   dog
 end
 
