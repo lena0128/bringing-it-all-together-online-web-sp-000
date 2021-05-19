577a5efb4ebc2f5f0fbb9e647817516fbaf7f9c7
Lena Hou <lenahou2013@gmail.com>
2021-05-19 21:00:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -37,6 +37,13 @@ def save
     DB[:conn].execute(sql, self.name, self.breed)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
   end
+
+def self.create(name:, breed:)
+  dog = Dog.new(name, album)
+  dog.save
+  dog
+end
+
 end
 
 
